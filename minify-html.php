007e208a194d2b46123389b2b1519aef8256180c
Awilum <awilum@msn.com>
2018-11-20 17:34:47 +0300

    Minify Html 1.0.1

--- minify-html.php
+++ minify-html.php
@@ -36,19 +36,10 @@ class MinifyHTML
      */
     public static function process($html)
     {
-        // Remove HTML comments (not containing IE conditional comments).
-        $html = preg_replace_callback('/<!--([\\s\\S]*?)-->/', 'MinifyHTML::_comments', $html);
-
         // Trim each line.
         $html = preg_replace('/^\\s+|\\s+$/m', '', $html);
 
         // Return HTML
         return $html;
     }
-
-    protected static function _comments($m)
-    {
-        return (0 === strpos($m[1], '[') || false !== strpos($m[1], '<![')) ? $m[0] : '';
-    }
-
 }
